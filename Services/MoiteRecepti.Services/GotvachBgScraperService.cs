1df5930f3d3093c1ad774453780db43d1e3727ba
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-02-01 19:46:51 +0200

    Create class Scrapper
    update

---
+++ Services/MoiteRecepti.Services/GotvachBgScraperService.cs
@@ -0,0 +1,18 @@
+﻿namespace MoiteRecepti.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class GotvachBgScraperSevice: IGotvachBgScraper
+    {
+        public void PopulateDbWithRecipes(int fromId, int toId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    {
+    }
+}
