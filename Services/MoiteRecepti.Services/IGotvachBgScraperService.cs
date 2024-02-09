f83128df2dfb0d500425365343c50e0362510d46
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-02-09 09:03:50 +0200

    work with scraper
    update

--- Services/MoiteRecepti.Services/IGotvachBgScraperService.cs
+++ Services/MoiteRecepti.Services/IGotvachBgScraperService.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MoiteRecepti.Services
+﻿namespace MoiteRecepti.Services
 {
-    internal interface IGotvachBgScraper
+    using System.Threading.Tasks;
+
+    public interface IGotvachBgScraperService
     {
+        Task ImportRecipesAsync(int fromId = 1, int toId = 10000);
     }
 }
--- Services/MoiteRecepti.Services/Models/RecipeDto.cs
+++ Services/MoiteRecepti.Services/Models/RecipeDto.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MoiteRecepti.Services.Models
+﻿namespace MoiteRecepti.Services.Models
 {
-    internal class GotvachBgDto
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class RecipeDto
     {
+        public RecipeDto()
+        {
+            this.Ingredients = new List<string>();
+        }
+
+        public string CategoryName { get; set; }
+
+        public string RecipeName { get; set; }
+
+        public string Instructions { get; set; }
+
+        public TimeSpan PreparationTime { get; set; }
+
+        public TimeSpan CookingTime { get; set; }
+
+        public int PortionsCount { get; set; }
+
+        public string OriginalUrl { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public List<string> Ingredients { get; set; }
     }
 }
